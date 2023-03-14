ed2beb317d50f73f2349a3da887ac13f60103e7e
zinakalu <zina.kalu@gmail.com>
2023-03-14 05:52:27 -0400

    events lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+
+function handleFocus(event){
+  console.log("Good!")
+}
+
+function handleBlur(event){
+  console.log("Hey! Eyes on me!")
+}
+
+  return (
+   <button  onFocus={handleFocus}  onBlur={handleBlur} >
+    EyesOnMe
+   </button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,17 @@
 // Code Keypad Component Here
 
 function Keypad (){
+
+function handleChange(event){
+    console.log('Entering password')
+}
+
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={handleChange} >
+
+            </input>
+        </div>
     )
 }
 
